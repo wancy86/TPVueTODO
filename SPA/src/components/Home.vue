a034e0bcd4505e53c201682d64c73c56f4308f0e
Mark <wancy86@sina.com>
2017-07-25 18:52:36 +0800

    ss

--- SPA/src/components/Home.vue
+++ SPA/src/components/Home.vue
@@ -2,10 +2,24 @@
   <app-layout>
     <div class="row">
       <div class="col-md-8 col-md-offset-2">
-          <h3>Home is here</h3>
-          <h4>{{name}}</h4>
-          <input type="text" :value="name" @change="changeName()">
-          <input type="text" v-model="name">
+          <h3>Issue List</h3>
+          
+          <div class="form-group col-sm-10" style="padding-left:0px;">
+            <input v-model="IssueDesc" type="text" class="form-control" placeholder="Issue Descrition">
+          </div>
+          <div class="form-group col-sm-2" style="padding-right:0px;">
+            <button @click="addIssue" class="btn btn-success" type="button"><span class="glyphicon glyphicon-plus"></span> Add</button>
+          </div>
+
+          
+          <div class="list-group col-sm-12" v-for="(issue,index) in IssueList">
+            <a style="background-color:#5cb85c;" href="#" class="list-group-item">{{index+1}}. {{issue.IssueDesc}}</a>
+            <a href="#" class="list-group-item" v-for="(comment,index) in issue.comments">{{index+1}}. {{comment}}</a>
+            <div class="input-group">
+              <input type="text" class="form-control" placeholder="comments">
+              <div style="sursor:pointer" class="input-group-addon">Comment</div>
+            </div>
+          </div>
       </div>
     </div>
   </app-layout>
@@ -20,7 +34,9 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js home',
-      name:'test',      
+      name:'test',  
+      IssueDesc: '',
+      IssueList: [{IssueDesc:"test issue",comments:['123','123345','22222']}],
     }
   },
   components: {AppLayout},
@@ -32,7 +48,18 @@ export default {
         }).catch(function  (err) {
           console.log('XXXX: ',err)
         })
-      }
+      },
+    addIssue: function  () {
+      console.log('XXXX: ',this.IssueDesc)
+      this.IssueList.push({
+        IssueDesc:this.IssueDesc,
+        comments:[]
+      });
+    }
+  },
+  created: function  () {
+    //get init data
+    console.log('XXXX: ','created')
   }
 }
 </script>
